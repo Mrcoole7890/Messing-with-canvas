e27cfef4dfdb87635b16adb62f52107bd1f924eb
Cole Kendall <kendallcole7@gmail.com>
2021-04-01 05:01:57 -0400

    Add blank test functions for "HeadOnCollisions"

---
+++ sideProject/test.js
@@ -0,0 +1,20 @@
+var testing = {
+  collisionTesting: function() {
+    //HeadOnCollisionDetection will test if, when two visualObjects, possesing same size coliding surfaces,
+    //that they do not overlap OR fail to touch.
+    function HeadOnCollisionDetectionLeft() {
+
+    }
+    function HeadOnCollisionDetectionRight() {
+
+    }
+
+    function HeadOnCollisionDetectionTop() {
+
+    }
+
+    function HeadOnCollisionDetectionBottom() {
+
+    }
+  }
+};
