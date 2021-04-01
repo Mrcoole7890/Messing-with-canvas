e8ab87ce4339d0dfc740a66cb3b382d806d27f43
Cole Kendall <kendallcole7@gmail.com>
2021-04-01 05:37:02 -0400

    Add "OffSet" collision detection testing

--- sideProject/test.js
+++ sideProject/test.js
@@ -16,5 +16,89 @@ var testing = {
     function HeadOnCollisionDetectionBottom() {
 
     }
+
+    //   |=======|
+    //   |=======|
+    //   |=======|
+    //   |=======|
+    //       \=======\ (Target Oject)
+    //       \=======\
+    //       \=======\
+    //       \=======\
+    function OffSetCollisionDetectionTopLeft() {
+
+    }
+    //       \=======\
+    //       \=======\
+    //       \=======\
+    //       \=======\
+    //   |=======| (Target Oject)
+    //   |=======|
+    //   |=======|
+    //   |=======|
+    function OffSetCollisionDetectionTopRight() {
+
+    }
+    //       \=======\ (Target Oject)
+    //       \=======\
+    //       \=======\
+    //       \=======\
+    //   |=======|
+    //   |=======|
+    //   |=======|
+    //   |=======|
+    function OffSetCollisionDetectionBottomLeft() {
+
+    }
+    //   |=======| (Target Oject)
+    //   |=======|
+    //   |=======|
+    //   |=======|
+    //       \=======\
+    //       \=======\
+    //       \=======\
+    //       \=======\
+    function OffSetCollisionDetectionBottomRight() {
+
+    }
+    //                  |=======|
+    //                  |=======|
+    //                  |=======|\=======\ (Target Object)
+    //                  |=======|\=======\
+    //                           \=======\
+    //                           \=======\
+    //
+    function OffSetCollisionDetectionLeftTop() {
+
+    }
+    //                           \=======\ (Target Object)
+    //                           \=======\
+    //                  |=======|\=======\
+    //                  |=======|\=======\
+    //                  |=======|
+    //                  |=======|
+    function OffSetCollisionDetectionLeftBottom() {
+
+    }
+    //
+    //                           \=======\
+    //                           \=======\
+    //   (Target Object)|=======|\=======\
+    //                  |=======|\=======\
+    //                  |=======|
+    //                  |=======|
+    function OffSetCollisionDetectionRightTop() {
+
+    }
+    //   (Target Object)|=======|
+    //                  |=======|
+    //                  |=======|\=======\
+    //                  |=======|\=======\
+    //                           \=======\
+    //                           \=======\
+    //
+    function OffSetCollisionDetectionRightBottom() {
+
+    }
   }
 };
